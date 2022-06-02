b9faffc3064e9ce6c2a1284939abcd4b22fd461c
Jason McGinty <39670976+JasonMcGinty@users.noreply.github.com>
2022-06-02 16:48:57 -0400

    Create SQL-Recover-System-DBs.ps1

---
+++ SQL-Recover-System-DBs.ps1
@@ -0,0 +1,3 @@
+License: MIT
+$filePath = "C:\Program Files\Microsoft SQL Server\150\Setup Bootstrap\SQL2019\setup.exe"
+Start-Process -FilePath $filePath -ArgumentList "/QUIET /ACTION=REBUILDDATABASE /INSTANCENAME=SQL2019 /SQLSYSADMINACCOUNTS=jason";
